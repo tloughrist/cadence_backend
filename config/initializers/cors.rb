583a0923579238f6dcf60d4bea26f073c110cd4a
timloughrist <tim.loughrist@gmail.com>
2023-05-12 04:30:28 -0500

    update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://cadence.onrender.com/"
+    origins "https://cadence.onrender.com"
   
     resource "*",
       headers: :any,
